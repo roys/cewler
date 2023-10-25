5781a6f008be445be1e735253caed61e833d3a67
Roy Solberg <git@roysolberg.com>
2023-10-25 21:51:01 +0200

    Bumped version.

--- src/cewler/cewler.py
+++ src/cewler/cewler.py
@@ -27,7 +27,7 @@ except ImportError:  # When running this file directly
 
 
 __author__ = "Roy Solberg"
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 __program__ = "CeWLeR"
 __description__ = "Custom Word List generator Redefined"
 
