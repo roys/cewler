675c2aaea010ecddcab4a9d47661244eef6c53a9
Roy Solberg <git@roysolberg.com>
2025-04-12 21:26:05 +0200

    \+ Added support for application/json (always enabled, no flags)

--- src/cewler/cewler.py
+++ src/cewler/cewler.py
@@ -27,7 +27,7 @@ except ImportError:  # When running this file directly
 
 
 __author__ = "Roy Solberg"
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 __program__ = "CeWLeR"
 __description__ = "Custom Word List generator Redefined"
 
